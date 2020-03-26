141b6ca810d1810cdee110df8dc657aefba111af
Omolola Odunowo <omololaodunowo@gmail.com>
2020-03-26 22:56:19 +0100

    I am adding my first Javascript task on StartNg

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function jsTask(){
+    let name='Omolola Odunowo';
+    let courses=['HTML','CSS', 'JavaScript'];
+    let i;
+    
+
+        for (i=0; i < 200; i+=2){
+            if (courses.length % 2 === 0){
+                document.write(i + " ");
+                    } 
+                    for (i=1; i < 200; i+=2){
+                        if(courses.length % 2 !== 0){
+
+                         document.write(i + " ");
+                        }
+                    }
+}
+
+}
+
+jsTask(); //It will output odd numbers since the number of my courses are 3
